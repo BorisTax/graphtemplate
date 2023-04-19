2bb5d433b604b0f999b35269c93f0ee313f9e49b
BorisTax <tboris1983@gmail.com>
2023-04-19 11:11:42 +0300

    PanHandler corrected

--- src/handlers/PanHandler.tsx
+++ src/handlers/PanHandler.tsx
@@ -1,6 +1,5 @@
-import Geometry from "../utils/geometry";
 import { MouseHandler } from "./MouseHandler";
-import { getRealRect, getScreenRect, setCurCoord, setTopLeft } from "../functions/viewPortFunctions";
+import { setCurCoord, setTopLeft } from "../functions/viewPortFunctions";
 import { Point } from "../types/properties";
 import { IHandler, TMouseProps, TTouchProps } from "../interfaces/HandlerInterface";
 import { Actions } from "../atoms/actions";
@@ -19,18 +18,12 @@ export class PanHandler extends MouseHandler implements IPanHandler {
         super.move(props);
         let dx = this.curPoint.x - this.startPoint.x;
         let dy = this.curPoint.y - this.startPoint.y;
-        const realRect = getRealRect(viewPortData.topLeft, viewPortData.bottomRight)
-        const screenRect = getScreenRect(viewPortData.viewPortWidth, viewPortData.viewPortHeight)
-        const screenTopLeft = Geometry.realToScreen({ x: dx, y: viewPortData.maxPanHeight }, realRect, screenRect);
-        const screenBottomRight = Geometry.realToScreen({ x: viewPortData.maxPanWidth + dx, y: 0 }, realRect, screenRect);
         const topLeft = { x: 0, y: 0 }
-        const xInRange = ((screenTopLeft.x < viewPortData.viewPortWidth / 2) && (dx > 0)) || ((screenBottomRight.x > viewPortData.viewPortWidth / 2) && (dx < 0));
-        const yInRange = ((screenTopLeft.y < viewPortData.viewPortHeight / 2) && (dy < 0)) || ((screenBottomRight.y > viewPortData.viewPortHeight / 2) && (dy > 0));
-        topLeft.x = xInRange ? viewPortData.topLeft.x - dx : viewPortData.topLeft.x;
-        topLeft.y = yInRange ? viewPortData.topLeft.y - dy : viewPortData.topLeft.y;
-        setViewPortData(setTopLeft(topLeft, viewPortData))
-        const cursorPoint = { x: xInRange ? this.startPoint.x : this.curPoint.x, y: yInRange ? this.startPoint.y : this.curPoint.y }
-        setViewPortData(setCurCoord(cursorPoint, curPoint, viewPortData))
+        topLeft.x = viewPortData.topLeft.x - dx;
+        topLeft.y = viewPortData.topLeft.y - dy;
+        const newData = setTopLeft(topLeft, viewPortData)
+        const cursorPoint = { x: this.startPoint.x, y: this.startPoint.y }
+        setViewPortData(setCurCoord(cursorPoint, curPoint, newData))
 
     }
     up(props: TMouseProps) {
